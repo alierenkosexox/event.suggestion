f4bb16ef871f6aea1d2026ed69f982c40b5e8ca8
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-22 04:45:21 +0300

    push

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int heat;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hava Kaç C° ?");
+        heat = input.nextInt();
+        
+        if (heat<5) {
+            System.out.println("Kayak Yapabilirsiniz");
+        } else if (heat<25) {
+            if (heat<=15) {
+                System.out.println("Sineymaya Gidebilirsiniz");
+            }
+            if (heat >= 10) {
+                System.out.println("Pikniğe gidebilirisinz.");
+            }
+        } else {
+            System.out.println("Hava sıcaklığı denize gitmeye uygun");
         }
     }
-}
+} // Ali Eren KÖSE 22/02/2024 04:59
